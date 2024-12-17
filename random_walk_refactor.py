3cdace3aeb1ee47b80805d3ceebd5b5750672e18
John Roddy <johnroddy16@yahoo.com>
2024-12-16 20:24:54 -0800

    random rgb color function

--- random_walk_refactor.py
+++ random_walk_refactor.py
@@ -2,28 +2,38 @@
 
 # we will write the random walk a slightly different way with less code:
 
-from turtle import Turtle as t, Screen as s 
+import turtle as t  
 import random 
 
-alex = t()
+alex = t.Turtle()
 alex.pensize(10)  # larger pen size 
 alex.speed(0)  # 0 is the fastest speed, could also write 'fastest' 
 
-colors = ['BlueViolet', 'powder blue', 'cadet blue', 'chartreuse', 'dark slate gray', 'DarkSeaGreen', 'DeepPink2', 'coral4', 'LightCyan1', 'LightGreen',
-          'LightSkyBlue2', 'medium spring green', 'MediumOrchid3', 'PaleGreen']  # leave the colors the same 4 now 
+# colors = ['BlueViolet', 'powder blue', 'cadet blue', 'chartreuse', 'dark slate gray', 'DarkSeaGreen', 'DeepPink2', 'coral4', 'LightCyan1', 'LightGreen',
+#           'LightSkyBlue2', 'medium spring green', 'MediumOrchid3', 'PaleGreen']  # leave the colors the same 4 now 
+
+# Set color mode to RGB
+t.colormode(255)
+
+# changing the way we create random colors with a function that returns r, g, b tuple:
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return r, g, b
 
 directions = [0, 90, 180, 270]  # 0 east, 90 north, 180 west, 270 south
 
 for _ in range(1000):
+    alex.color(random_color()) 
     alex.setheading(random.choice(directions))
-    alex.color(random.choice(colors))
     alex.forward(20)
 
 
 
 
 
-screen = s()
+screen = t.Screen()
 screen.title('random walk')
 screen.bgcolor('skyblue')
 screen.exitonclick()
